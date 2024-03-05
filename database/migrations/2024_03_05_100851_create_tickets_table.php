143580bb315eff4aeb8a6053c1d208f36585555a
AllaouiJihad <allaouijihad05@gmail.com>
2024-03-05 11:28:50 +0100

    la migration des tableaux ->ticket ->event ->categorie

---
+++ database/migrations/2024_03_05_100851_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->float('price', 8, 2);
+            $table->integer('places_nbr');
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
